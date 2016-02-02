e408adbeccaa77a81a068c6b461b663447e64c1d
hyuntae.cho <hyuntae.cho@navercorp.com>
2016-02-02 14:21:55 +0900

    NaverCafeSDK 1.2.0

---
+++ sample/naversdk-sample-unity5/Assets/NCSDK/Plugins/iOS/NaverCafeSDK.framework/Versions/Current/Headers/NCSDKColor.h
@@ -0,0 +1,23 @@
+//
+//  NCSDKColor.h
+//  glink_sdk
+//
+//  Created by Gyeonghwan on 2015. 12. 1..
+//  Copyright (c) 2015년 Naver. All rights reserved.
+//
+
+#ifndef glink_sdk_NCSDKColor_h
+#define glink_sdk_NCSDKColor_h
+
+typedef NS_ENUM(NSUInteger, GLColorDefine) {
+    kGLColorWhite1 = 0xFFFFFF,
+    kGLColorGray1 = 0xCCCCCC,
+    kGLColorGray3 = 0xAFB4BA,
+    kGLColorGray4 = 0x999999,
+    kGLColorBlack1 = 0x44484E,
+    kGLColorBlack2 = 0x000000,
+    kGLColorGreen1 = 0x00D650,
+    kGLColorBlue2 = 0x2E65D9,
+};
+
+#endif
