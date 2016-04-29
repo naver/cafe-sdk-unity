e723bedf7696be979d16dcb15f8bcd107508f5ea
hyuntae.cho <hyuntae.cho@navercorp.com>
2016-04-29 12:21:58 +0900

    Release 1.5.0

---
+++ sample/navercafesdk-sample-unity5/Assets/NCSDK/Sample/SampleBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.IO;
+
+public class SampleBehaviour : MonoBehaviour {
+	public void OnClickGlinkButton () {
+		GLink.sharedInstance().executeHome ();
+	}
+	
+	public void OnClickScreenShotButton () {
+		StartCoroutine (this.CoFunction ());
+		
+		Texture2D image = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
+		image.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, true);
+		image.Apply ();
+		Debug.Log (image);
+		
+		byte[] bytes = image.EncodeToPNG();
+		string path = Application.persistentDataPath + "/GLShareImage.png";
+		File.WriteAllBytes(path, bytes);		
+
+		GLink.sharedInstance().executeArticlePostWithImage(10, "스크린샷!!", "제 점수는요~", path);
+	}
+
+	IEnumerator CoFunction () {
+		yield return new WaitForEndOfFrame();
+	}
+}
+
+
