67fdde3ed3048e8f7505378a990dd6acb04c16d2
hyuntae.cho <hyuntae.cho@navercorp.com>
2016-04-29 12:01:43 +0900

    remove 1.4.0 sample

--- sample/navercafesdk-sample-unity4/Assets/Scripts/GLinkConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿static class GLinkConfig
-{
-	public const string NaverLoginClientId = "U5ZHoj_OStOHOJ8mec_s";
-	public const string NaverLoginClientSecret = "piPHPA9i4E";
-	public const int CafeId = 28334359;
-}
