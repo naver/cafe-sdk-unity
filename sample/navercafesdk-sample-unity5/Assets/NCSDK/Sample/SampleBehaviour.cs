533bd926c66e017f48a48ce4ae6b916562bbaf02
hyuntae.cho <hyuntae.cho@navercorp.com>
2016-10-14 10:46:00 +0900

    NaverCafeSDK 1.7.4

---
+++ sample/navercafesdk-sample-unity5/Assets/NCSDK/Sample/SampleBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.IO;
+
+public class SampleBehaviour : MonoBehaviour {
+	public void OnClickGlinkButton () {
+		GLink.sharedInstance().executeHome ();
+	}
+	
+	public void OnClickScreenShotButton () {
+		StartCoroutine (SaveScreenShot ());
+	}
+
+	// http://wiki.unity3d.com/index.php/ScreenCapture
+	private IEnumerator SaveScreenShot () {
+		yield return new WaitForEndOfFrame();
+
+		string filePath = Application.persistentDataPath + "/GLShareImage.png";
+
+		//Create a texture to pass to encoding
+		Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+		//Put buffer into texture
+		texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+
+		//Split the process up--ReadPixels() and the GetPixels() call inside of the encoder are both pretty heavy
+		yield return 0;
+
+		byte[] bytes = texture.EncodeToPNG();
+
+		//Save our test image (could also upload to WWW)
+		File.WriteAllBytes(filePath, bytes);
+
+		//Tell unity to delete the texture, by default it seems to keep hold of it and memory crashes will occur after too many screenshots.
+		DestroyObject(texture);
+
+		GLink.sharedInstance().executeArticlePostWithImage(10, "스크린샷!!", "제 점수는요~", filePath);
+	}
+}
+
+
