466c7a91bed7cb305b8a725076812cb950730540
hyuntae.cho <hyuntae.cho@navercorp.com>
2016-11-08 14:07:40 +0900

    PLUG SDK 2.1.0

--- sample/navercafesdk-sample-unity4/Assets/Scripts/GLinkConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿static class GLinkConfig
-{
-	public const string NaverLoginClientId = "UKvNABLDsyEJusJGsXL2";
-	public const string NaverLoginClientSecret = "rK4suc_Qd0";
-	public const int CafeId = 28266581;
-}
