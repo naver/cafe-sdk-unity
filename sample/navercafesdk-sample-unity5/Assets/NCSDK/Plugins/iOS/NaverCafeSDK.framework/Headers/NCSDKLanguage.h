ad45ea7988354f32482cbe8a063d1f3c1b23cb45
hyuntae.cho <hyuntae.cho@navercorp.com>
2018-01-31 14:57:03 +0900

    Release 3.2.0 (iOS)

---
+++ sample/navercafesdk-sample-unity5/Assets/NCSDK/Plugins/iOS/NaverCafeSDK.framework/Headers/NCSDKLanguage.h
@@ -0,0 +1,34 @@
+//
+//  GLLanguageCode.h
+//  glink_sdk
+//
+//  Created by Naver on 2016. 8. 16..
+//  Copyright © 2016년 Naver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString *const KOREAN = @"ko";
+static NSString *const ENGLISH = @"en";
+static NSString *const CHINESE_SIMPLIFIED = @"zh_CN";
+static NSString *const CHINESE_TRADITIONAL = @"zh_TW";
+static NSString *const JAPANESE = @"ja";
+static NSString *const THAI = @"th";
+static NSString *const GERMAN = @"de";
+static NSString *const FRENCH = @"fr";
+static NSString *const SPANISH = @"es";
+static NSString *const INDONESIAN = @"in";
+static NSString *const RUSSIAN = @"ru";
+
+static NSString *const VIETNAMESE = @"vi";
+static NSString *const ITALIAN = @"it";
+static NSString *const PORTUGUESE = @"pt";
+static NSString *const TURKISH = @"tr";
+static NSString *const MEXICAN  = @"es_MX";
+static NSString *const BRAZILIAN = @"pt_BR";
+
+
+
+
+
+
